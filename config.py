6dc30afed7e4e791339b5472e5565e70de28b10a
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-24 18:07:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
+API_ID = (getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7396541413))
+OWNER_ID = (getenv("OWNER_ID", 7396541413))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PBX_CHAT")
 
 MUST_JOIN = getenv("MUST_JOIN", "PBX_CHAT")
